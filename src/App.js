da6acf4fbccfb846870242e470d776ea47203914
DADA YO <dayeon.yoon@gmail.com>
2022-07-24 21:25:53 +0900

    STYLE: Add box style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import styled from "styled-components";
 
+const Parent = styled.div`
+  display: flex;
+`;
+
+const BoxTeal = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+
+const BoxTomato = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span``;
+
 function App() {
-  return <div></div>;
+  return (
+    <Parent>
+      <BoxTeal>
+        <Text>Hello</Text>
+      </BoxTeal>
+      <BoxTomato></BoxTomato>
+    </Parent>
+  );
 }
 
 export default App;
