6cef18c5a49cd005bbdb62073819436117ea8962
DADA YO <dayeon.yoon@gmail.com>
2022-07-25 23:25:48 +0900

    STYLE: Component Expending

--- src/App.js
+++ src/App.js
@@ -4,27 +4,21 @@ const Parent = styled.div`
   display: flex;
 `;
 
-const BoxTeal = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTomato = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
-
-const Text = styled.span``;
 
 function App() {
   return (
     <Parent>
-      <BoxTeal>
-        <Text>Hello</Text>
-      </BoxTeal>
-      <BoxTomato></BoxTomato>
+      <Box bgColor="teal"></Box>
+      <Circle bgColor="tomato"></Circle>
     </Parent>
   );
 }
