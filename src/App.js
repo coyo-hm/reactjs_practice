1638d90d4793c072b731e42c9c340d225e7b5405
DADA YO <dayeon.yoon@gmail.com>
2022-07-26 01:14:40 +0900

    F

--- src/App.js
+++ src/App.js
@@ -1,55 +1,19 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0%;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 50%;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 0%;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
+`;
 
-  ${Emoji} {
-    &:hover {
-      font-size: 50px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
-  return (
-    <Wrapper>
-      <Box>
-        <Emoji>😁</Emoji>
-      </Box>
-    </Wrapper>
-  );
+  return <Wrapper></Wrapper>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+
+const darkTheme = {
+  //theme애 어떤 색을 사용할 건지 구체적으로 결정된다.
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
+const lightTheme = {
+  //theme애 어떤 색을 사용할 건지 구체적으로 결정된다.
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
