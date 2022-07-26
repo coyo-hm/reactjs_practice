90267bb2672ddc908187819a3154f104dc2d0c13
DADA YO <dayeon.yoon@gmail.com>
2022-07-26 23:55:10 +0900

    STYLE: Add Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [username, setUserName] = useState("");
-  const onChangeName = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setUserName(value);
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("hello" + username);
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={username}
-          onChange={onChangeName}
-          type="text"
-          placeholder="username"
-        />
-        <button>LOGIN</button>
-      </form>
-    </div>
+    <Container>
+      <H1>HELLO</H1>
+    </Container>
   );
 }
 
