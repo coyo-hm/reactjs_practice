3410ff79c5cd77917c9da64984fddfffd929857e
DADA YO <dayeon.yoon@gmail.com>
2022-07-26 23:39:37 +0900

    FEAT: Add Login Btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [username, setUserName] = useState("");
+  const onChangeName = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    setUserName(value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello" + username);
+  };
+
   return (
     <div>
-      <Circle bgColor={"teal"} borderColor={"black"} />
-      <Circle bgColor={"tomato"} />
+      <form onSubmit={onSubmit}>
+        <input
+          value={username}
+          onChange={onChangeName}
+          type="text"
+          placeholder="username"
+        />
+        <button>LOGIN</button>
+      </form>
     </div>
   );
 }
