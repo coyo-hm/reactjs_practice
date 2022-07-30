c1cd932938021b30a755ecee5ecb41508e4d534a
DADA YO <dayeon.yoon@gmail.com>
2022-07-31 00:32:22 +0900

    FEAT: Fetch Coin info & price data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { Container, Header, Loader, Title } from "../style";
-
 import { CoinInterface } from "../interface";
 
 interface LocationInterface {
@@ -11,7 +10,26 @@ interface LocationInterface {
 function Coin() {
   const { coinId } = useParams();
   const { state: coin } = useLocation() as LocationInterface;
+
   const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      setLoading(false);
+    })();
+  }, []);
 
   return (
     <Container>
