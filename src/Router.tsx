9dd7de4ed0c45bc04faa4d6e5e32bfd6f1e37ce0
DADA YO <dayeon.yoon@gmail.com>
2022-07-31 00:16:50 +0900

    FEAT: Send Coin data to coin.tsx from coins.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -16,7 +16,7 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/:coinid" element={<Coin />} />
+        <Route path="/:coinId" element={<Coin />} />
         <Route path="/*" element={<Coins />} />
       </Routes>
     </BrowserRouter>
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,9 +1,26 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { Container, Header, Loader, Title } from "../style";
+
+import { CoinInterface } from "../interface";
+
+interface LocationInterface {
+  state: CoinInterface;
+}
 
 function Coin() {
   const { coinId } = useParams();
+  const { state: coin } = useLocation() as LocationInterface;
+  const [loading, setLoading] = useState(true);
 
-  return <h1>Coin : {coinId}</h1>;
+  return (
+    <Container>
+      <Header>
+        <Title>{coin?.name || "Loading"}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 }
 
 export default Coin;
