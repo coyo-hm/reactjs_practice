56481e18df4fbde1378ac35a3813799c13e78138
DADA YO <dayeon.yoon@gmail.com>
2022-08-02 00:51:25 +0900

    FEAT: Get coinHistory data

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,4 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { fetchCoinHistory } from "../api";
+
 function Chart() {
+  const { coinId } = useParams();
+
+  const { isLoading, data } = useQuery(["price", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+
   return <h1>Chart</h1>;
 }
 export default Chart;
