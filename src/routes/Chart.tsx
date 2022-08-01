f9b40224b10360334745c1ea901988e819e3e0e9
DADA YO <dayeon.yoon@gmail.com>
2022-08-02 01:06:26 +0900

    BUILD: Add ApexCahrt

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,11 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
 
 function Chart() {
   const { coinId } = useParams();
 
-  const { isLoading, data } = useQuery(["price", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["price", coinId], () =>
     fetchCoinHistory(coinId)
   );
 
