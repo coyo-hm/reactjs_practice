be3629f455d3406a66c0397fd0e4faf8e7a9999b
DADA YO <dayeon.yoon@gmail.com>
2022-08-06 19:38:56 +0900

    FEAT: Make ToDoList

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,4 +1,31 @@
+import React, { useState } from "react";
+
 function ToDoList() {
-  return null;
+  const [toDo, setToDo] = useState("");
+
+  const onChangeValue = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    setToDo(value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(toDo);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChangeValue}
+          value={toDo}
+          placeholder={"Write a to do"}
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 export default ToDoList;
