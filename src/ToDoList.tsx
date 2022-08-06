db4ab85dcf95d21167d2fc1c370a517ff682258c
DADA YO <dayeon.yoon@gmail.com>
2022-08-06 20:52:51 +0900

    REFACTOR: Use useForm

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,40 +2,54 @@ import React, { useState } from "react";
 import { darkTheme } from "./style/theme";
 import { useForm } from "react-hook-form";
 
+// function ToDoList() {
+//   const [toDo, setToDo] = useState("");
+//   const [toDoError, setToDoError] = useState("");
+
+//   const onChangeValue = (e: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = e;
+//     setToDoError("");
+//     setToDo(value);
+//   };
+
+//   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     if (toDo.length < 10) {
+//       return setToDoError("To do should be longer");
+//     }
+//     console.log(toDo);
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input
+//           onChange={onChangeValue}
+//           value={toDo}
+//           placeholder={"Write a to do"}
+//         />
+//         <button>Add</button>
+//         {toDoError !== "" ? (
+//           <span style={{ color: `${darkTheme.textColor}` }}>{toDoError}</span>
+//         ) : null}
+//       </form>
+//     </div>
+//   );
+// }
+
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const [toDoError, setToDoError] = useState("");
-
-  const onChangeValue = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setToDoError("");
-    setToDo(value);
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (toDo.length < 10) {
-      return setToDoError("To do should be longer");
-    }
-    console.log(toDo);
-  };
+  const { register } = useForm();
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChangeValue}
-          value={toDo}
-          placeholder={"Write a to do"}
-        />
+      <form>
+        <input {...register("toDo")} placeholder={"Write a to do"} />
         <button>Add</button>
-        {toDoError !== "" ? (
-          <span style={{ color: `${darkTheme.textColor}` }}>{toDoError}</span>
-        ) : null}
       </form>
     </div>
   );
 }
+
 export default ToDoList;
