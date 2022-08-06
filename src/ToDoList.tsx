6da03e3677f703a2c9474c18c36257a64d5e4560
DADA YO <dayeon.yoon@gmail.com>
2022-08-06 20:39:14 +0900

    FEAT: Add input error

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
+import { darkTheme } from "./style/theme";
 
 function ToDoList() {
   const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
 
   const onChangeValue = (e: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = e;
+    setToDoError("");
     setToDo(value);
   };
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (toDo.length < 10) {
+      return setToDoError("To do should be longer");
+    }
     console.log(toDo);
   };
 
@@ -24,6 +30,9 @@ function ToDoList() {
           placeholder={"Write a to do"}
         />
         <button>Add</button>
+        {toDoError !== "" ? (
+          <span style={{ color: `${darkTheme.textColor}` }}>{toDoError}</span>
+        ) : null}
       </form>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
-import { darkTheme } from "./theme";
+import { darkTheme } from "./style/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
