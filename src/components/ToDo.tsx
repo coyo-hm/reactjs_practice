4be4e7ff6c62a12c0a470c667f8400fe0c2db30b
DADA YO <dayeon.yoon@gmail.com>
2022-08-09 21:38:31 +0900

    FEAT: Category button event

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,29 @@
+import React from "react";
 import { IToDo } from "../atoms";
 
 function ToDo({ text, id, category }: IToDo) {
+  const onClickCategory = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const { currentTarget: name } = event;
+  };
+
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClickCategory}>
+          To Do
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClickCategory}>
+          Doing
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClickCategory}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
