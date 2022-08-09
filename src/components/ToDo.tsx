141a6153b0e19fede8f7eaa978abde8621c9f188
DADA YO <dayeon.yoon@gmail.com>
2022-08-09 21:21:35 +0900

    FEAT: Add Category Button

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,7 +1,14 @@
 import { IToDo } from "../atoms";
 
 function ToDo({ text, id, category }: IToDo) {
-  return <li>{toDo.text}</li>;
+  return (
+    <li>
+      <span>{text}</span>
+      <button>To Do</button>
+      <button>Doing</button>
+      <button>Done</button>
+    </li>
+  );
 }
 
 export default ToDo;
