a7dafc3146bcecd822abe016255edd5c43ff62b8
DADA YO <dayeon.yoon@gmail.com>
2022-08-17 00:55:32 +0900

    BUILD: Init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,5 @@
-import React from "react";
-import { useRecoilState } from "recoil";
-import { hourSelector, minuteState } from "./atoms";
-
 function App() {
-  const [minutes, setMinutess] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourSelector);
-
-  const onChangeMinutes = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutess(+event.currentTarget.value);
-  };
-
-  const onChangeHours = (event: React.FormEvent<HTMLInputElement>) => {
-    setHours(+event.currentTarget.value);
-  };
-
-  return (
-    <div>
-      <input
-        type="number"
-        placeholder="Minutes"
-        value={minutes}
-        onChange={onChangeMinutes}
-      />
-      <input
-        type="number"
-        placeholder="Hours"
-        value={hours}
-        onChange={onChangeHours}
-      />
-    </div>
-  );
+  return;
 }
 
 export default App;
