3824701f71c987971dc253d5d7601bbc8a8709d7
DADA YO <dayeon.yoon@gmail.com>
2022-08-20 02:24:33 +0900

    STYLE: Keep board height

--- src/App.tsx
+++ src/App.tsx
@@ -63,6 +63,7 @@ function App() {
                     )}
                   </Draggable>
                 ))}
+                {provided.placeholder}
               </Board>
             )}
           </Droppable>
