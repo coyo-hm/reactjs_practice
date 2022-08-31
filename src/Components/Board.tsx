6524322947c97ae262acaf6a7d8b8b47806cbd68
DADA YO <dayeon.yoon@gmail.com>
2022-09-01 01:43:56 +0900

    FEAT: Change board color by card state

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -7,12 +7,31 @@ export const Wrapper = styled.div`
   background-color: ${(prop) => prop.theme.boardColor};
   border-radius: 5px;
   min-height: 200px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h1`
   text-align: center;
   margin-bottom: 15px;
   font-weight: 700;
+`;
+
+interface IAreaProps {
+  isDraggingOver: boolean;
+  isDraggingFromThis: boolean;
+}
+
+const Area = styled.div<IAreaProps>`
+  background-color: ${(props) =>
+    props.isDraggingOver
+      ? "#95afc0"
+      : props.isDraggingFromThis
+      ? "#f9ca24"
+      : ""};
+  flex-grow: 1;
+  border-radius: 5px;
+  transition: background-color 0.3s ease-in-out;
 `;
 
 interface IBoardProps {
@@ -25,13 +44,18 @@ function Board({ toDos, boardId }: IBoardProps) {
     <Wrapper>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
+        {(provided, snapshot) => (
+          <Area
+            isDraggingOver={snapshot.isDraggingOver}
+            isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
             {toDos.map((toDo, idx) => (
               <DraggableCard toDo={toDo} idx={idx} key={toDo} />
             ))}
             {provided.placeholder}
-          </div>
+          </Area>
         )}
       </Droppable>
     </Wrapper>
