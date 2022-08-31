2875f9dd749389e320a0c194ebb6458da93f5a65
DADA YO <dayeon.yoon@gmail.com>
2022-09-01 00:57:23 +0900

    FEAT: Move a card in the same board

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,25 @@ import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import { toDoState } from "./atoms";
 import Board from "./Components/Board";
-import DraggableCard from "./Components/DraggableCard";
 import { Container, Boards } from "./style";
 
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
 
-  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
-    if (!destination) return;
-    // setToDos((prevToDos) => {
-    //   const copyToDos = [...prevToDos];
-    //   copyToDos.splice(source.index, 1);
-    //   copyToDos.splice(destination?.index, 0, draggableId);
-    //   return copyToDos;
-    // });
+  const onDragEnd = (info: DropResult) => {
+    const { destination, draggableId, source } = info;
+    if (destination?.droppableId === source.droppableId) {
+      //같은 보드 내에서 움직인다면
+      setToDos((allBoards) => {
+        const boardCopy = [...allBoards[source.droppableId]];
+        boardCopy.splice(source.index, 1);
+        boardCopy.splice(destination?.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: boardCopy,
+        };
+      });
+    }
   };
 
   return (
--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -4,10 +4,14 @@ import DraggableCard from "./DraggableCard";
 
 export const Wrapper = styled.div`
   padding: 20px 10px;
-  padding-top: 30px;
   background-color: ${(prop) => prop.theme.boardColor};
   border-radius: 5px;
   min-height: 200px;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 15px;
 `;
 
 interface IBoardProps {
@@ -17,16 +21,19 @@ interface IBoardProps {
 
 function Board({ toDos, boardId }: IBoardProps) {
   return (
-    <Droppable droppableId={boardId}>
-      {(provided) => (
-        <Wrapper ref={provided.innerRef} {...provided.droppableProps}>
-          {toDos.map((toDo, idx) => (
-            <DraggableCard toDo={toDo} idx={idx} key={toDo} />
-          ))}
-          {provided.placeholder}
-        </Wrapper>
-      )}
-    </Droppable>
+    <Wrapper>
+      <Title>{boardId}</Title>
+      <Droppable droppableId={boardId}>
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            {toDos.map((toDo, idx) => (
+              <DraggableCard toDo={toDo} idx={idx} key={toDo} />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </Wrapper>
   );
 }
 
