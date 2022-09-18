acdc24e623bde49649b9e6b1eda5023e3ee00dc3
DADA YO <dayeon.yoon@gmail.com>
2022-09-18 21:34:12 +0900

    FEAT: Variant

--- src/components/Variant.tsx
+++ src/components/Variant.tsx
@@ -1,19 +1,60 @@
 import styled from "styled-components";
-import { Wrapper as BoxWrapper, Box } from "../style";
+import { Wrapper as BoxWrapper, Box as DefaultBox } from "../style";
 
 const Wrapper = styled(BoxWrapper)`
-  background: linear-gradient(135deg, #651d83, #b468d2);
+  background: linear-gradient(135deg, #e365f4, #9a01bc);
 `;
 
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 270, transition: { type: "spring", delay: 1 } },
+const Box = styled(DefaultBox)`
+  background-color: rgba(255, 255, 255, 0.2);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding: 12px;
+`;
+
+const Circle = styled(DefaultBox)`
+  background-color: white;
+  height: 25px;
+  width: 25px;
+  border-radius: 50%;
+  place-self: center;
+`;
+
+const boxVariant = {
+  start: { opacity: 0, scale: 0.5 },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariant = {
+  start: {
+    opacity: 0,
+    y: 10, // Framer Motion에만 존재하는 x, y
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
 };
 
 function Variant() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={boxVariant} initial="start" animate="end">
+        <Circle variants={circleVariant} />
+        <Circle variants={circleVariant} />
+        <Circle variants={circleVariant} />
+        <Circle variants={circleVariant} />
+      </Box>
     </Wrapper>
   );
 }
