5a52ce429b5e931942bce167d400835fe0915cca
DADA YO <dayeon.yoon@gmail.com>
2022-11-06 04:25:17 +0900

    FEAT: Animation Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,47 @@
 import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
-  display: flex;
-  justify-content: center;
+  display: grid;
+  justify-items: center;
   align-items: center;
-
-  .prev {
-    margin-right: 250px;
-  }
-
-  .next {
-    margin-left: 250px;
-  }
+  column-gap: 50px;
+  grid-template-columns: repeat(2, 1fr);
 `;
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  position: absolute;
-  top: calc(50% - 100px);
-  display: flex;
+  border-radius: 50px;
   justify-content: center;
   align-items: center;
+  display: flex;
   font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const box = {
-  entry: (isBack: boolean) => {
-    return {
-      x: isBack ? -500 : 500,
-      opacity: 0,
-      scale: 0,
-    };
-  },
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.3,
-    },
-  },
-  exit: (isBack: boolean) => {
-    return {
-      x: isBack ? 500 : -500,
-      opacity: 0,
-      scale: 0,
-      transition: { duration: 0.3 },
-    };
-  },
-};
+const Circle = styled(motion.div)`
+  background-color: #00a6ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-  const movePrev = () => {
-    setBack(true);
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => {
+    setClicked((prev) => !prev);
   };
-  const moveNext = () => {
-    setBack(false);
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  };
+
   return (
-    <Wrapper>
-      <button className="prev" onClick={movePrev}>
-        PREV
-      </button>
-      <AnimatePresence custom={back}>
-        <Box
-          custom={back}
-          variants={box}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button className="next" onClick={moveNext}>
-        NEXT
-      </button>
+    <Wrapper onClick={toggleClicked}>
+      <Box>{!clicked && <Circle layoutId="circle" />}</Box>
+      <Box>{clicked && <Circle layoutId="circle" />}</Box>
     </Wrapper>
   );
 }
