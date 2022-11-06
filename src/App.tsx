720d81534970dc396e14267017ef4f7c43739940
DADA YO <dayeon.yoon@gmail.com>
2022-11-07 00:14:49 +0900

    FEAT: Search Bar UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./routes/Home";
+import Search from "./routes/Search";
+import TV from "./routes/TV";
+
+const SITE_URL = {
+  HOME: "/",
+  TV: "/tv",
+  SEARCH: "/search",
+};
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path={SITE_URL.HOME} element={<Home />} />
+        <Route path={SITE_URL.TV} element={<TV />} />
+        <Route path={SITE_URL.SEARCH} element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
