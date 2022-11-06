07d7a494844c70148c99663856da60479db53b02
DADA YO <dayeon.yoon@gmail.com>
2022-11-06 20:18:26 +0900

    BUILD: Project Initalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RecoilRoot } from "recoil";
-
-const queryClient = new QueryClient();
+import "./style/reset.css";
+import { theme } from "./style/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
