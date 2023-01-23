3f1fb45641ae6cf845e4cf7af889b753e9119d5a
DADA YO <dayeon.yoon@gmail.com>
2023-01-24 04:36:34 +0900

    Box Animation 2

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -70,6 +70,20 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
   }
 `;
 
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
 const rowVariants = {
   hidden: {
     x: window.outerWidth + DEFAULT_GAP,
@@ -80,12 +94,19 @@ const rowVariants = {
   exit: { x: -window.outerWidth - DEFAULT_GAP },
 };
 
-const BoxVariants = {
+const boxVariants = {
   normal: { scale: 1 },
   hover: {
     scale: 1.3,
     y: -50,
-    transition: { delay: 0.5, duration: 0.3, type: "tween" },
+    transition: { delay: 0.5, duration: 0.1, type: "tween" },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: { delay: 0.5, duration: 0.1, type: "tween" },
   },
 };
 
@@ -146,12 +167,21 @@ function Home() {
                   .map((movie) => (
                     <Box
                       key={movie.id}
-                      variants={BoxVariants}
+                      variants={boxVariants}
                       initial="normal"
                       whileHover="hover"
                       transition={{ type: "tween" }}
                       bgPhoto={makeImagePath(movie.backdrop_path || "", "w500")}
-                    />
+                    >
+                      {/* <img
+                        src={makeImagePath(movie.backdrop_path || "", "w500")}
+                        alt={movie.title}
+                        title={movie.title}
+                      /> */}
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
+                      </Info>
+                    </Box>
                   ))}
               </Row>
             </AnimatePresence>
