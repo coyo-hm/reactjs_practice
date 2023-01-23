32be457fee27622180394a4bf494ca9947d3b18e
DADA YO <dayeon.yoon@gmail.com>
2023-01-24 03:21:04 +0900

    Slider 2

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,6 +5,8 @@ import { makeImagePath } from "../utils";
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
+const DEFAULT_GAP = 5;
+
 const Wrapper = styled.div`
   /* background-color: whitesmoke; */
   background-color: black;
@@ -47,25 +49,30 @@ const Slider = styled.div`
 const Row = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(6, 1fr);
-  gap: 10px;
+  gap: ${DEFAULT_GAP + "px"};
   position: absolute;
   width: 100%;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{ bgPhoto: string }>`
   background-color: white;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
   height: 200px;
 `;
 
 const rowVariants = {
   hidden: {
-    x: window.outerWidth,
+    x: window.outerWidth + DEFAULT_GAP,
   },
   visible: {
     x: 0,
   },
-  exit: { x: -window.outerWidth - 10 },
+  exit: { x: -window.outerWidth - DEFAULT_GAP },
 };
+
+const DEFAULT_PAGE_SIZE = 6;
 
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
@@ -74,8 +81,23 @@ function Home() {
   );
 
   const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
 
-  const increaseIndex = () => setIndex((prev) => prev + 1);
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) {
+        return;
+      }
+      setLeaving(true);
+      const totalMovies = data.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / DEFAULT_PAGE_SIZE) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+
+  const toggleLeaving = () => {
+    setLeaving((prev) => !prev);
+  };
 
   console.log(data, isLoading);
   return (
@@ -89,7 +111,7 @@ function Home() {
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
-            <AnimatePresence>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
               <Row
                 variants={rowVariants}
                 initial="hidden"
@@ -98,9 +120,18 @@ function Home() {
                 transition={{ type: "tween", duration: 1 }}
                 key={index}
               >
-                {[1, 2, 3, 4, 5, 6].map((i) => (
-                  <Box key={i} />
-                ))}
+                {data?.results
+                  .slice(1)
+                  .slice(
+                    DEFAULT_PAGE_SIZE * index,
+                    DEFAULT_PAGE_SIZE * index + DEFAULT_PAGE_SIZE
+                  )
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      bgPhoto={makeImagePath(movie.backdrop_path || "", "w500")}
+                    />
+                  ))}
               </Row>
             </AnimatePresence>
           </Slider>
